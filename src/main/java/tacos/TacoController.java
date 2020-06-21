98d6266d376b6c9536af74c231caa1cb2a9588bc
AndreiMarchenko <andreymarchanka@gmail.com>
2020-06-22 01:02:20 +0300

    Initial project

---
+++ src/main/java/tacos/TacoController.java
@@ -0,0 +1,10 @@
+package tacos;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class TacoController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
